fc809430f517fa8a58ae7812de0283e842295d57
freddax <jana.hennig@online.de>
2016-07-04 20:46:13 +0200

    Janas Test-Branch Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  #validates :name, :city, :country, length: {minimum: 2}
+  validates :age, numericality: { only_integer: true}
+  validates :email, format: { with: /\w+@\w+/ }, presence: true
+  #validates :website, format: { with: /https?:\/\/\w+\.\w+/ }
+  validates :website, format: { with: /\Ahttps?:\/\/.+\..+/ }
 end
