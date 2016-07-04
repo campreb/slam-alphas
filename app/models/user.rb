fd203eeaf473d5f6ca97c13c789adde1f0acbf59
Dayana Mick <dayanamick@gmail.com>
2016-07-04 20:27:58 +0200

    add validation format for url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  #validates :name, :city, :country, :email, :age, presence: true
+  validates :website, format: { with: /\A(http|https):\/\/[a-z0-9]+([\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\z/ }
 end
